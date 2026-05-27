5f1901cafb00b5cb2804876ae6600c5ec5d00423
Aman Raj <a.raj4426005@gmail.com>
2026-05-28 02:55:32 +0530

    Initial project upload

---
+++ app_disease.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("KNN_heart.pkl")
+scaler = joblib.load("heart_scaler.pkl")
+expected_column = joblib.load("heart_columns.pkl")
+
+st.title("Heart Stroke preidctio by AMAN")
+st.markdown("Provide the following details")
+
+age = st.slider("Age",18,100,40)
+sex = st.selectbox("Sex",['M','F'])
+chest_pain = st.selectbox("Chest Pain Type",["ATA","NAP","TA","ASY"])
+resting_bp = st.number_input("resting Blood Pressure (mm Hg)",80,120,100)
+cholesterol = st.number_input("Cholesterol (mm/dL)",100,600,200)
+fasting_BS = st.selectbox("Fasting Blood Sugar > 120 mm/dL", [0,1])
+resting_ecg = st.selectbox("Resting ECG",["Normal","ST","LVH"])
+max_hr = st.slider("Max_Heart_rate",60,220,150)
+exercise_angina = st.selectbox("Exercise-Induced_Angins",["Y","N"])
+oldpeak = st.slider("OldPeak (ST Depression)",0.0,6.0,1.0)
+st_slope = st.selectbox("ST_Slope",["Up","Flat","Down"])
+
+
+if st.button("Predict"):
+
+        raw_input = {
+        'Age': age,
+        'RestingBP': resting_bp,
+        'Cholesterol': cholesterol,
+        'FastingBS': +fasting_BS,
+        'MaxHR': max_hr,
+        'Oldpeak': oldpeak,
+        'Sex_' + sex: 1,
+        'ChestPainType_' + chest_pain: 1,
+        'RestingECG_' + resting_ecg: 1,
+        'ExerciseAngina_' + exercise_angina: 1,
+        'ST_Slope_' + st_slope: 1
+    }
+        
+        input_df = pd.DataFrame([raw_input])
+
+        for col in expected_column:
+            if col not in input_df.columns:
+               input_df[col] = 0
+        
+        
+        input_df = input_df[expected_column]
+
+        scaled_input = scaler.transform(input_df)
+
+        prediction = model.predict(scaled_input)[0]
+
+        if prediction == 1:
+          st.error("⚠️ High Risk of Heart Disease")
+        else:
+          st.success("✅ Low Risk of Heart Disease")
